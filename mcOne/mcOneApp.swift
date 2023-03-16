af30bd60a75ca4daee96ffa58038403663295f5f
luthfiasmara <luthfigooners29@gmail.com>
2023-03-16 11:27:50 +0700

    Initial Commit

---
+++ mcOne/mcOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcOneApp.swift
+//  mcOne
+//
+//  Created by Luthfi Asmara on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct mcOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
