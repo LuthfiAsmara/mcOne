7e487de10a67bb12ea47238f95412b964f81c38c
luthfiasmara <luthfigooners29@gmail.com>
2023-04-11 08:53:41 +0700

    edit view penjelasan penjumlahan

--- mcOne/widgets/explanationAdd.swift
+++ mcOne/widgets/explanationAdd.swift
@@ -16,150 +16,190 @@ struct ExplanationAddView : View{
 //        self.destination = destination
 //    }
     var body: some View{
-        ZStack {
-            // Set background image
-            Image("bg")
-                .resizable()
-                .edgesIgnoringSafeArea(.all)
-                .blur(radius: 8)
-            VStack(alignment: .leading){
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 120, height: 40)
-                    .foregroundColor(Color(red: 235/255, green: 194/255, blue: 151/255))
-                    .overlay(Text("Penjelasan")
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0)))
+        let firstColumns = Int(ceil(sqrt(Double(num1))))
+                let firstRows = Int(ceil(Double(num1) / Double(firstColumns)))
+                let secondColumns = Int(ceil(sqrt(Double(num2))))
+                let secondRows = Int(ceil(Double(num2) / Double(secondColumns))) //
+                let thirdColumns = Int(ceil(sqrt(Double(ans))))
+                let thirdRows = Int(ceil(Double(ans) / Double(thirdColumns))) //
                 ZStack {
-                    Color(red: 255/255, green: 236/255, blue: 160/255)
-                    VStack(alignment: .leading) {
-                        HStack {
-                            VStack{
-                                Text("\(num1)")
-                                    .font(.headline)
-                                    .fontWeight(.medium)
-                                    .foregroundColor(Color(red: 0.351, green: 0.288, blue: 0.223))
-                                ZStack{
-                                    Color(red: 255/255, green: 249/255, blue: 223/255)
-                                    HStack{
-                                        Spacer()
-                                        ForEach(0..<num1, id: \.self){ _ in
-                                            Image("donat")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .padding(.vertical)
-                                            Spacer()
+                    // Set background image
+                    Image("bg")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .blur(radius: 8)
+                    VStack(alignment: .leading){
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 120, height: 40)
+                            .foregroundColor(Color(red: 235/255, green: 194/255, blue: 151/255))
+                            .overlay(Text("Penjelasan")
+                                .font(.headline)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0)))
+                        ZStack {
+                            Color(red: 255/255, green: 236/255, blue: 160/255)
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    VStack{
+                                        ZStack{
+                                            Color(red: 255/255, green: 249/255, blue: 223/255)
+                                            VStack(spacing: 0) {
+                                                ForEach(0..<firstRows, id: \.self) { row in
+                                                    HStack(spacing: 0) {
+                                                        ForEach(0..<firstColumns, id: \.self) { column in
+                                                            let index = row * firstColumns + column
+                                                            if index < num1 {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)
+                                                                    .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 1, x: -2, y: 4)
+                                                                
+                                                                
+                                                            } else {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)                               .hidden()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            .padding()
                                         }
+                                                                        .frame(width: 150, height: 100)
+                                                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                                                        .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
+
+                                    }
+                                    VStack{
+        //                                Text(" ")
+                                        Rectangle()
+                                            .foregroundColor(Color(red: 255/255, green: 249/255, blue: 223/255))
+                                            .frame(width: 40, height: 40)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
+                                            .overlay(
+                                            Text("+")
+                                                .font(.headline)
+                                                .fontWeight(.medium)
+                                                .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
+
+                                            )
+
+                                    }
+                                    VStack{
+        //                                Text("\(num2)")
+        //                                    .font(.headline)
+        //                                    .fontWeight(.medium)
+        //                                    .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
+        //
+                                        ZStack{
+                                            Color(red: 255/255, green: 249/255, blue: 223/255)
+                                            VStack(spacing: 0) {
+                                                ForEach(0..<secondRows, id: \.self) { row in
+                                                    HStack(spacing: 0) {
+                                                        ForEach(0..<secondColumns, id: \.self) { column in
+                                                            let index = row * secondColumns + column
+                                                            if index < num2 {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)
+                                                                    .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 1, x: -2, y: 4)
+                                                                
+                                                            } else {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)                               .hidden()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            .padding()
+                                        }
+                                                                        .frame(width: 150, height: 100)
+                                                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                                                        .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
+
+
+                                    }
+                                    Spacer()
+                                }
+                                Spacer()
+
+                                HStack(){
+                                    Spacer()
+                                    VStack(){
+        
+                                        Rectangle()
+                                            .foregroundColor(Color(red: 235/255, green: 194/255, blue: 151/255))
+                                            .frame(width: 40, height: 40)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
+                                            .overlay(
+                                            Text("=")
+                                                .font(.headline)
+                                                .fontWeight(.medium)
+                                                .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
+
+                                            )
+
+                                    }
+                                    VStack{
+                                        ZStack{
+                                            Color(red: 255/255, green: 249/255, blue: 223/255)
+                                            VStack(spacing: 0) {
+                                                ForEach(0..<thirdRows, id: \.self) { row in
+                                                    HStack(spacing: 0) {
+                                                        ForEach(0..<thirdColumns, id: \.self) { column in
+                                                            let index = row * thirdColumns + column
+                                                            if index < ans {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)
+                                                                    .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 1, x: -2, y: 4)
+                                                                
+                                                                
+                                                            } else {
+                                                                Image("donat")
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .padding(3)                               .hidden()
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            .padding()
+                                        }
+                                                                        .frame(width: 300, height: 120)
+                                                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                                                        .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
                                     }
                                 }
-                                .frame(width: 150, height: 60)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
 
                             }
-                            VStack{
-                                Text(" ")
-                                Rectangle()
-                                    .foregroundColor(Color(red: 255/255, green: 249/255, blue: 223/255))
-                                    .frame(width: 40, height: 40)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
-                                    .overlay(
-                                    Text("+")
-                                        .font(.headline)
-                                        .fontWeight(.medium)
-                                        .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
-
-                                    )
-
-                            }
-                            VStack{
-                                Text("\(num2)")
-                                    .font(.headline)
-                                    .fontWeight(.medium)
-                                    .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
-
-                                ZStack{
-                                    Color(red: 255/255, green: 249/255, blue: 223/255)
-                                    HStack{
-                                        Spacer()
-                                        ForEach(0..<num2, id: \.self){ _ in
-                                            Image("donat")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .padding(.vertical)
-                                            Spacer()
-                                        }
-                                    }
-                                }
-                                .frame(width: 150, height: 60)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
-
-
-                            }
-                            Spacer()
+                            .frame(width: 350, height: 250)
+                            .offset(x:-70)
                         }
-                        
-                        HStack {
-                            VStack{
-                                Text(" ")
-                                Rectangle()
-                                    .foregroundColor(Color(red: 235/255, green: 194/255, blue: 151/255))
-                                    .frame(width: 40, height: 40)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
-                                    .overlay(
-                                    Text("=")
-                                        .font(.headline)
-                                        .fontWeight(.medium)
-                                        .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
-
-                                    )
-
-                            }
-                            VStack{
-                                Text("\(ans)")
-                                    .font(.headline)
-                                    .fontWeight(.medium)
-                                    .foregroundColor(Color(red: 0.349, green: 0.288, blue: 0.224, opacity: 100.0))
-
-                                ZStack{
-                                    Color(red: 255/255, green: 249/255, blue: 223/255)
-                                    HStack{
-                                        Spacer()
-                                        ForEach(0..<ans, id: \.self){ _ in
-                                            Image("donat")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .padding(.vertical)
-                                            Spacer()
-                                        }
-                                    }
-                                }
-                                .frame(width: 310, height: 60)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .shadow(color: Color(red: 96/255, green: 96/255, blue: 96/255, opacity: 0.1), radius: 2, x: -2, y: 4)
-
-                            }
-                        }
-
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .frame(width: 600, height: 280)
                     }
-                    .frame(width: 500, height: 200)
+                    Image("teacher")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 400, height: 400, alignment: .trailing)
+                        .offset(x: 200, y: 30)
                 }
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .frame(width: 600, height: 250)
-            }
-            Image("teacher")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 400, height: 400, alignment: .trailing)
-                .offset(x: 200, y: 30)
-        }
     }
 }
 struct ExplanationAddView_Previews: PreviewProvider {
     static var previews: some View {
-        ExplanationAddView(num1: 3, num2: 2, ans: 5)
+        ExplanationAddView(num1: 5, num2: 4, ans: 9)
     }
 }
